ef73010676f25a45bd49be9736aa34709dbc1a21
Nayama Rosa <nayama@masma.com.br>
2024-11-14 13:55:43 -0300

    ajustando configurações e layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import styles from "./layout.module.css";
-import Link from "next/link";
+import { NavLinks } from "./ui/nav-links";
 
-const harmRegular = localFont({
-  src: "./fonts/Harmattan-Regular.ttf",
-  variable: "--regular",
-  weight: "400",
-});
-const harmBold = localFont({
-  src: "./fonts/Harmattan-Regular.ttf",
-  variable: "--bold",
-  weight: "700",
-});
 
 export const metadata: Metadata = {
   title: "Nayama Rosa Portfolio",
@@ -25,21 +14,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="pt-BR">
-      <body className={harmRegular.variable}>
+      <body>
         <div className={styles.layout}>
           <main className={styles.layoutMain}>
             {children}
           </main>
-            <nav className={styles.layoutNav}>
-              <ul className={styles.layoutNavList}>
-                {/* <li className={`${styles.layoutNavListItem} ${styles.homeColor}`}><Link className={styles.layoutNavListItemLink} href="/"><span className={styles.layoutNavListText}>Nayama Rosa</span></Link></li> */}
-                <li className={`${styles.layoutNavListItem} ${styles.devColor}`}><Link className={styles.layoutNavListItemLink} href="/development"><span className={styles.layoutNavListText}>Programação</span></Link></li>
-                <li className={`${styles.layoutNavListItem} ${styles.designColor}`}><Link className={styles.layoutNavListItemLink} href="/design"><span className={styles.layoutNavListText}>Design</span></Link></li>
-                <li className={`${styles.layoutNavListItem} ${styles.contactColor}`}><Link className={styles.layoutNavListItemLink} href="/contact"><span className={styles.layoutNavListText}>Contato</span></Link></li>
-              </ul>
-          </nav>
+          <NavLinks />
         </div>
       </body>
     </html>
